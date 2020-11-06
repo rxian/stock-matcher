4da2eb407010c6ca925d4d5197ebb82ae023ddf9
Louise15926 <yangwx01@gmail.com>
2020-11-05 18:44:00 -0600

    set up react router

--- web/src/index.js
+++ web/src/index.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Switch, useParams } from 'react-router-dom';
 import './index.css';
-import App from './app/App';
+import StockSearch from './search/StockSearch';
 import Admin from './admin/Admin';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
     <BrowserRouter>
-        <Route exact path="/" component={App}/>
-        <Route path="/admin" component={Admin}/>
+        <Switch>
+            <Route exact path="/admin" component={Admin}/>
+            <Route path="/">
+                <StockSearch/>
+                <Switch>
+                    <Route path={"/listing/:id"}>
+                        <Child/>
+                    </Route>
+                </Switch>
+            </Route>
+        </Switch>
     </BrowserRouter>,
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
+function Child() {
+    // We can use the `useParams` hook here to access
+    // the dynamic pieces of the URL.
+    let { id } = useParams();
+
+    return (
+        <div>
+            <h3>ID: {id}</h3>
+        </div>
+    );
+}
+
+// If you want to start measuring performance in your search, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
