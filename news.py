1bf08719b9f67ac76e4cc3beeb395a0ef7a8359f
Steven Li <levensti@gmail.com>
2020-11-22 17:42:47 -0600

    Create news object class

---
+++ news.py
@@ -0,0 +1,8 @@
+class News:
+    def __init__(self, id, timestamp, title, url):
+        self.id = id
+        self.timestamp = timestamp 
+        self.title = title
+        self.url = url
+    
+    
