3be8d073a2eb4cb6e229b2b2cf8cc171c3bc08e0
Steven Li <levensti@gmail.com>
2020-11-22 17:26:31 -0600

    Update requirements.txt

--- generate_sp_500_list.py
+++ generate_sp_500_list.py
@@ -2,16 +2,20 @@ import bs4 as bs
 import requests
 
 sp_500_companies = []
-resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-soup = bs.BeautifulSoup(resp.text, 'lxml')
-table = soup.find('table', {'class': 'wikitable sortable'})
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
-resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies',
-                    headers=headers)
+def generate():
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
 
-for row in table.findAll('tr')[1:]:
-    company = row.findAll('td')[1].text
-    sp_500_companies.append(company.replace("\n", ""))
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies',
+                        headers=headers)
 
-print(sp_500_companies)
+    for row in table.findAll('tr')[1:]:
+        company = row.findAll('td')[1].text
+        sp_500_companies.append(company.replace("\n", ""))
+
+    return sp_500_companies
+
+generate()
