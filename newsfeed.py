0c200f2b03f34d3e9fcdd0693609dec657d58641
Steven Li <levensti@gmail.com>
2020-11-22 16:14:44 -0600

    Use Spacy NLP to gather company entities and return associated companies in news articles

--- newsfeed.py
+++ newsfeed.py
@@ -1,6 +1,11 @@
 from credentials import news_api_key
 import json 
 import requests
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+nlp = en_core_web_sm.load()
 
 endpoint = "https://newsapi.org/v2/top-headlines?language=en&country=us&"
 
@@ -15,4 +20,24 @@ def get_articles():
 
     return descriptions
 
+# takes a block of text and returns a list of companies mentioned in the piece
+def extract_orgs(description):
+    tokens = nlp(description)
+    companies = []
+    for t in tokens.ents:
+        if t.label_ == 'ORG':
+            companies.append(t.text)
 
+    return companies
+
+# parameters:
+# content list -- The list of descriptions from the news of the day
+# sp_500_companies -- The sp_500 companies (detect tickers or company name)
+# return: a list of related companies for the day
+def news_related_tickers(article_descriptions, sp_500_companies):
+    related_tickers = []
+    for i in range(0, len(article_descriptions)):
+        # at each iteration, add a list of lists of related companies
+        related_tickers.append(extract_orgs)
+
+    return related_tickers
