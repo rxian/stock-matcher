78ac0b7e644f542320c6d9c9997e1a98d6b981d2
gandi2 <gandi2@illinois.edu>
2020-11-24 02:57:13 -0500

    changed date handling

--- neo4j/query_graph.py
+++ neo4j/query_graph.py
@@ -17,14 +17,15 @@ def query_num_connections_by_date(tx, company1, company2, schema, date):
         "WHERE a.{attribute1} = $company1 AND b.{attribute2} = $company2 AND article.timestamp.epochSeconds >= dateRange.epochSeconds " 
         "RETURN count(*) AS Connections"
     ).format(attribute1 = schema[0], attribute2 = schema[1])
-    return tx.run(query, years = date[2], days = date[1], months = date[0], company1 = company1, company2 = company2).single().value()
+    return tx.run(query, years = date["years"], days = date["days"], months = date["months"], company1 = company1, company2 = company2).single().value()
 
 def get_num_connections(company1, company2, schema = ['name', 'name'], date = None):
     # schema is a list of size 2 that specifies the attributes that identify the companies that are passed in (either through 'name' or 'symbol')
     # by default, it is set to identify both companies by name
 
-    # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format [month, date, year] where each element in the 
-    # list is a positive integer specifying how far back to look for relationships (subject to change)
+    # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format 
+    # {"month": something, "date": something, "year": something} where each value in the 
+    # dict is a positive integer specifying how far back to look for relationships (subject to change)
     with driver.session() as session:
         if not date:
             return session.read_transaction(query_num_connections, company1, company2, schema)
@@ -46,13 +47,14 @@ def query_connection_list_by_date(tx, company, attribute, date):
         "WHERE a.{id} = $company AND article.timestamp.epochSeconds >= dateRange.epochSeconds "
         "RETURN collect(b.symbol) AS Connections"
     ).format(id = attribute)
-    return tx.run(query, years = date[2], days = date[1], months = date[0], company = company).single().value()
+    return tx.run(query, years = date["years"], days = date["days"], months = date["months"], company = company).single().value()
 
 def get_list_connections(company, attribute = 'name', date = None):
     # If company name is True, treat the parameter as a name (default), otherwise treat it as a symbol
 
-    # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format [month, date, year] where each element in the 
-    # list is a positive integer specifying how far back to look for relationships (subject to change)
+    # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format 
+    # {"month": something, "date": something, "year": something} where each value in the 
+    # dict is a positive integer specifying how far back to look for relationships (subject to change)
     with driver.session() as session:
         if not date:
             return session.read_transaction(query_connection_list, company, attribute)
@@ -74,13 +76,14 @@ def query_url_list_by_date(tx, company, attribute, date):
         "WHERE a.{id} = $company AND article.timestamp.epochSeconds >= dateRange.epochSeconds "
         "RETURN collect(b.url) AS URL_List"
     ).format(id = attribute)
-    return tx.run(query, years = date[2], days = date[1], months = date[0], company = company).single().value()
+    return tx.run(query, years = date["years"], days = date["days"], months = date["months"], company = company).single().value()
 
 def get_url_list(company, attribute = 'name', date = None):
     # If company name is True, treat the parameter as a name (default), otherwise treat it as a symbol
 
-    # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format [month, date, year] where each element in the 
-    # list is a positive integer specifying how far back to look for relationships (subject to change)
+    # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format 
+    # {"month": something, "date": something, "year": something} where each value in the 
+    # dict is a positive integer specifying how far back to look for relationships (subject to change)
     with driver.session() as session:
         if not date:
             return session.read_transaction(query_url_list, company, attribute)
