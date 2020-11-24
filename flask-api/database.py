d122bb4acb57753c149bdac06e2392451461a8ab
Louise15926 <yangwx01@gmail.com>
2020-11-23 18:27:12 -0600

    minor refactor on backedn

--- flask-api/database.py
+++ flask-api/database.py
@@ -10,3 +10,8 @@ NEO4J_DATABASE_URL = os.environ['_NEO4J_DATABASE_URL']
 NEO4J_DATABASE_USERNAME = os.environ['_NEO4J_DATABASE_USERNAME']
 NEO4J_DATABASE_PASSWORD = os.environ['_NEO4J_DATABASE_PASSWORD']
 driver = GraphDatabase.driver(NEO4J_DATABASE_URL, auth=basic_auth(NEO4J_DATABASE_USERNAME, str(NEO4J_DATABASE_PASSWORD)))
+
+
+def connect_db():
+    conn = mysql.connect()
+    return conn.cursor()
