ab17b12b8b96e17948c89071589faf220af3bde5
Louise15926 <yangwx01@gmail.com>
2020-11-24 01:12:06 -0600

    use env to set up api url

--- web/src/api.jsx
+++ web/src/api.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_STAGE === 'development'
+    ? "http://157.245.91.247:5000"
+    : "http://127.0.0.1:5000";
+
 export default axios.create({
-    // baseURL: "http://127.0.0.1:5000",
-    baseURL: "http://157.245.91.247:5000",
+    baseURL: baseURL,
     timeout: 10000,
 })
