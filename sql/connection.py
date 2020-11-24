e465c6cfedc83a4033c447c35d09707cfe491b26
Steven Li <levensti@gmail.com>
2020-11-24 15:37:20 -0600

    Remove unused files

--- sql/connection.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-import os.path
-
-host = 'mysql+pymysql://root@localhost:33003'
-database = 'stock_matcher'
-options = 'charset=utf8mb4'
-
-engine = sqlalchemy.create_engine(os.path.join(host,database)+'?'+options)
--- web/src/api.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    // baseURL: "http://127.0.0.1:5000",
-    baseURL: "http://157.245.91.247:5000",
-    timeout: 10000,
-})
