35f0704c9a60df7fe14b7b9d1d16e80bc0f47022
Louise15926 <yangwx01@gmail.com>
2020-11-25 15:49:15 -0600

    fix typo

--- web/src/api.jsx
+++ web/src/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_STAGE === 'development'
-    ? "http://157.245.91.247:5000"
-    : "http://127.0.0.1:5000";
+    ? "http://127.0.0.1:5000"
+    : "http://157.245.91.247:5000";
 
 export default axios.create({
     baseURL: baseURL,
