9343a9b56f8cae789864deef9c1051cffe61dec5
gandi2 <gandi2@illinois.edu>
2020-11-25 01:13:24 -0500

    changed the way companies are gathered

---
+++ news/generate_listings.py
@@ -0,0 +1,33 @@
+import urllib.request
+
+def get_listings() -> dict:
+    nasdaq_symbols_companies = {}
+    file = urllib.request.urlopen("ftp://ftp.nasdaqtrader.com/symboldirectory/nasdaqlisted.txt")
+    for line in file:
+        decoded_line = line.decode("utf-8")
+        if "NASDAQ TEST STOCK" not in decoded_line and "Symbol|" not in decoded_line and "File Creation Time" not in decoded_line and "Nasdaq Symbology Test" not in decoded_line:
+            listing_info = decoded_line.split('|')
+            company = listing_info[1]
+            if " etf" in company.lower():
+                company = company[:company.lower().find(" ETF")]
+            if " index fund" in company.lower():
+                company = company[:company.lower().find(" index fund")]
+            if " - " in company:
+                company = company[:listing_info[1].find(" - ")]
+            nasdaq_symbols_companies[listing_info[0]] = company
+    file = urllib.request.urlopen("ftp://ftp.nasdaqtrader.com/symboldirectory/otherlisted.txt")
+    for line in file:
+        decoded_line = line.decode("utf-8")
+        if "NASDAQ TEST STOCK" not in decoded_line and "Symbol|" not in decoded_line and "File Creation Time" not in decoded_line and "Nasdaq Symbology Test" not in decoded_line:
+            listing_info = decoded_line.split('|')
+            company = listing_info[1]
+            if " etf" in company.lower():
+                company = company[:company.lower().find(" etf")]
+            if " index fund" in company.lower():
+                company = company[:company.lower().find(" index fund")]
+            if " common s" in company.lower():
+                company = company[:company.lower().find(" common s")]
+            if ". " in company:
+                company = company[:listing_info[1].find(". ") + 1]
+            nasdaq_symbols_companies[listing_info[0]] = company
+    return nasdaq_symbols_companies
--- news/news.py
+++ news/news.py
@@ -1,12 +1,26 @@
-from relate import *
-
 class News:
-    def __init__(self, url, description, title, timestamp):
+    title = ""
+    description = ""
+    mentioned_companies = []
+    timestamp = ""
+
+    def __init__(self, url, description, mentioned_companies, title, timestamp):
         # use URL as primary key
         self.url = url
         self.title = title
         self.description = description
-        self.mentioned_companies = extract_orgs(description)
+        self.mentioned_companies = mentioned_companies
         self.timestamp = timestamp
 
+    def get_title(self) -> str:
+        return self.title
+
+    def get_description(self) -> str:
+        return self.description
+
+    def get_mentioned_companies(self) -> list:
+        return self.mentioned_companies
+
+    def get_timestamp(self) -> str:
+        return self.timestamp
 
