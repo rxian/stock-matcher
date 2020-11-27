12ca1fc9b9e3d2f8b10f82231415a5bc3620e059
gandi2 <gandi2@illinois.edu>
2020-11-26 23:05:06 -0500

    modified existing queries and added new queries

--- neo4j/query_graph.py
+++ neo4j/query_graph.py
@@ -1,72 +1,110 @@
 from database import driver, GraphDatabase
 from typing import Optional
 
-# In these queries, returning only the aggregation is easier to handle as returning anything else would result in an empty result when there is nothing to aggregate
-
-def query_num_connections(tx, company1: str, company2: str, schema: list, date: Optional[dict] = None) -> int:
+def query_num_connections(tx, company1: int, company2: int, date: Optional[dict] = None) -> int:
     date_comparison = "AND article.timestamp.epochSeconds >= dateRange.epochSeconds "
     query = (
         "MATCH (a:Listing)-[:MENTIONED_IN]->(article:News)<-[:MENTIONED_IN]-(b:Listing) "
-        "WHERE a.{attribute1} = $company1 AND b.{attribute2} = $company2 {date_compare}"
+        "WHERE a.listing_id = $company1 AND b.listing_id = $company2 {date_compare}"
         "RETURN count(*) AS Connections"
-    ).format(attribute1 = schema[0], attribute2 = schema[1], date_compare = "" if not date else date_comparison)
+    ).format(date_compare = "" if not date else date_comparison)
     if not date:
         return tx.run(query, company1 = company1, company2 = company2).single().value()
     else:
         date_min = "WITH (datetime() - duration({years: $years, days: $days, months: $months})) AS dateRange "
         return tx.run(date_min + query, years = date["years"], days = date["days"], months = date["months"], company1 = company1, company2 = company2).single().value()
 
-def get_num_connections(company1: str, company2: str, schema: list = ['name', 'name'], date: Optional[dict] = None) -> int:
-    # schema is a list of size 2 that specifies the attributes that identify the companies that are passed in (either through 'name' or 'symbol')
-    # by default, it is set to identify both companies by name
-
+def get_num_connections(company1: int, company2: int, date: Optional[dict] = None) -> int:
     # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format 
-    # {"month": something, "date": something, "year": something} where each value in the 
+    # {"months": something, "days": something, "years": something} where each value in the 
     # dict is a positive integer specifying how far back to look for relationships (subject to change)
     with driver.session() as session:
-        return session.read_transaction(query_num_connections, company1, company2, schema, date)
+        return session.read_transaction(query_num_connections, company1, company2, date)
 
-def query_connection_list(tx, company: str, attribute: str, date: Optional[dict] = None) -> list:
+def query_connection_list(tx, company: int, limit: Optional[int] = None, date: Optional[dict] = None) -> list:
     date_comparison = "AND article.timestamp.epochSeconds >= dateRange.epochSeconds "
+    limit_query = " LIMIT $n"
     query = (
         "MATCH (a:Listing)-[:MENTIONED_IN]->(article:News)<-[:MENTIONED_IN]-(b:Listing) "
-        "WHERE a.{id} = $company {date_compare}"
-        "RETURN collect(distinct b.symbol) AS Connections"
-    ).format(id = attribute, date_compare = "" if not date else date_comparison)
+        "WHERE a.listing_id = $company {date_compare}"
+        "RETURN b.listing_id AS Listing_Id, b.symbol AS Symbol, count(distinct article.url) AS Connections "
+        "ORDER BY Connections DESC, b.listing_id{limit_query}"
+    ).format(date_compare = "" if not date else date_comparison, limit_query = "" if not limit else limit_query)
     if not date:
-        return tx.run(query, company = company).single().value()
+        return tx.run(query, company = company, n = limit).values()
     else:
         date_min = "WITH (datetime() - duration({years: $years, days: $days, months: $months})) AS dateRange "
-        return tx.run(date_min + query, years = date["years"], days = date["days"], months = date["months"], company = company).single().value()
+        return tx.run(date_min + query, years = date["years"], days = date["days"], months = date["months"], company = company, n = limit).values()
 
-def get_connection_list(company: str, attribute: str = 'name', date: Optional[dict] = None) -> list:
-    # If company name is True, treat the parameter as a name (default), otherwise treat it as a symbol
-
+def get_connection_list(company: int, limit: Optional[int] = None, date: Optional[dict] = None) -> list:
     # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format 
-    # {"month": something, "date": something, "year": something} where each value in the 
+    # {"months": something, "days": something, "years": something} where each value in the 
     # dict is a positive integer specifying how far back to look for relationships (subject to change)
     with driver.session() as session:
-        return session.read_transaction(query_connection_list, company, attribute, date)
+        return session.read_transaction(query_connection_list, company, limit, date)
 
-def query_url_list(tx, company: str, attribute: str, date: Optional[dict] = None) -> list:
+def query_article_list(tx, company: int, limit: Optional[int] = None, date: Optional[dict] = None) -> list:
     date_comparison = "AND article.timestamp.epochSeconds >= dateRange.epochSeconds "
+    limit_query = " LIMIT $n"
     query = (
         "MATCH (a:Listing)-[:MENTIONED_IN]->(article:News) "
-        "WHERE a.{id} = $company {date_compare}"
-        "RETURN collect(distinct article.url) AS URL_List"
-    ).format(id = attribute, date_compare = "" if not date else date_comparison)
+        "WHERE a.listing_id = $company {date_compare}"
+        "RETURN article.title AS Title, article.url AS URL "
+        "ORDER BY article.timestamp.epochSeconds DESC{limit_query}"
+    ).format(date_compare = "" if not date else date_comparison, limit_query = "" if not limit else limit_query)
     if not date:
-        return tx.run(query, company = company).single().value()
+        return tx.run(query, company = company, n = limit).values()
     else:
         date_min = "WITH (datetime() - duration({years: $years, days: $days, months: $months})) AS dateRange "
-        return tx.run(date_min + query, years = date["years"], days = date["days"], months = date["months"], company = company).single().value()
+        return tx.run(date_min + query, years = date["years"], days = date["days"], months = date["months"], company = company, n = limit).values()
 
-def get_url_list(company: str, attribute: str = 'name', date: Optional[dict] = None) -> list:
-    # If company name is True, treat the parameter as a name (default), otherwise treat it as a symbol
-
+def get_article_list(company: int, limit: Optional[int] = None, date: Optional[dict] = None) -> list:
     # date is None by defult (no date limit when querying), but if specified it is assumed to be in the format 
-    # {"month": something, "date": something, "year": something} where each value in the 
+    # {"months": something, "days": something, "years": something} where each value in the 
     # dict is a positive integer specifying how far back to look for relationships (subject to change)
     with driver.session() as session:
-        return session.read_transaction(query_url_list, company, attribute, date)
+        return session.read_transaction(query_article_list, company, limit, date)
+
+def query_similar_companies(tx, company: int, attribute: str, limit: Optional[int] = None) -> list:
+    limit_query = " LIMIT $n"
+    query = (
+        "MATCH (a:Listing), (b:Listing) "
+        "WHERE a.listing_id = $company AND a.{attr} = b.{attr} AND a.listing_id <> b.listing_id "
+        "RETURN b.listing_id AS Listing_Id, b.symbol AS Symbol "
+        "ORDER BY Listing_Id{limit_query}"
+    ).format(attr = attribute, limit_query = "" if not limit else limit_query)
+    return tx.run(query, company = company, n = limit).values()
+
+def get_similar_companies(company: int, attribute: str = "sector", limit: Optional[int] = None) -> list:
+    # attribute can either be "sector" or "industry"; the default is "sector"
+    with driver.session() as session:
+        return session.read_transaction(query_similar_companies, company, attribute, limit)
+
+def query_industry_sector_total_companies(tx, attribute: str, limit: Optional[int] = None) -> list:
+    limit_query = " LIMIT $n"
+    query = (
+        "MATCH (a:Listing) "
+        "RETURN a.{attr} AS {attr}, count(distinct a) AS Total "
+        "ORDER BY Total DESC{limit_query}"
+    ).format(attr = attribute, limit_query = "" if not limit else limit_query)
+    return tx.run(query, n = limit).values()
+
+def get_industry_sector_total_companies(attribute: str = "sector", limit: Optional[int] = None) -> list:
+    # attribute can either be "sector" or "industry"; the default is "sector"
+    with driver.session() as session:
+        return session.read_transaction(query_industry_sector_total_companies, attribute, limit)
+
+def query_industry_sector_total_market_cap(tx, attribute: str, limit: Optional[int] = None) -> list:
+    limit_query = " LIMIT $n"
+    query = (
+        "MATCH (a:Listing) "
+        "RETURN a.{attr} AS {attr}, sum(a.market_cap) AS Total "
+        "ORDER BY Total DESC{limit_query}"
+    ).format(attr = attribute, limit_query = "" if not limit else limit_query)
+    return tx.run(query, n = limit).values()
+
+def get_industry_sector_total_market_cap(attribute: str = "sector", limit: Optional[int] = None) -> list:
+    # attribute can either be "sector" or "industry"; the default is "sector"
+    with driver.session() as session:
+        return session.read_transaction(query_industry_sector_total_market_cap, attribute, limit)
 
